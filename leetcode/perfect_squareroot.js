b311ea56659e4a0d6652bebdf8964e92170401c5
Akash khot <akashkhot03@gmail.com>
2023-04-29 13:26:21 +0530

    perfect sqaure root

---
+++ leetcode/perfect_squareroot.js
@@ -0,0 +1,9 @@
+var isPerfectSquare = function(num) {
+    let root;
+    root=Math.sqrt(num)
+    res=Math.floor(root)
+    isSqrt=res*res==num?true:false
+    return isSqrt;
+    
+}
+console.log(isPerfectSquare(16))
