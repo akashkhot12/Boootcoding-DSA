6c6bc72ee5c2527065f6a308cf5a50350ecef710
Akash khot <akashkhot03@gmail.com>
2023-04-29 12:03:44 +0530

    problem solving on leetcode

---
+++ leetcode/Element_Sum_and_Digit_Sum.js
@@ -0,0 +1,15 @@
+// Input: nums = [1,15,6,3]
+// Output: 9
+// Explanation: 
+// The element sum of nums is 1 + 15 + 6 + 3 = 25.
+// The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+// The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+
+let nums =[1,15,6,3] 
+// let sum = 0 ; 
+// for(let i=0;i<nums.length;i++){
+//     sum += nums[i];
+// }
+// console.log(sum);
+nums.toString().split("")
+console.log(nums);
