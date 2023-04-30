29da7f2f65ef0d35e7decaeac07e503e1c86f8d2
Akash khot <akashkhot03@gmail.com>
2023-04-30 10:20:29 +0530

    solving

--- leetcode/Fizz_Buzz.js
+++ leetcode/Fizz_Buzz.js
@@ -1,11 +1,22 @@
 var fizzBuzz = function(n) {
     let array=[]
-    for (let index = 1; index <= n.length; index++) {
+    for (let index = 1; index <= 10; index++) {
         if (index % 3==0 && index % 5==0) {
             array.push("FizzBuzz")     
         }
-        
+        else if (index % 3 == 0){
+            array.push("Fizz") 
+        }
+        else if (index % 5 == 0){
+            array.push("Buzz") 
+        }
+        else{
+            array.push(index)
+        }
     }
-};
+        return array
+    
+    }
+    
 
-fizzBuzz(10)
+console.log(fizzBuzz(10))
