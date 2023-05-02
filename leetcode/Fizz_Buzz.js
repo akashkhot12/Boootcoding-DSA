76b95697a3de8229260d55d06cb55c303a7e1e3e
Akash khot <akashkhot03@gmail.com>
2023-05-02 14:27:44 +0530

    fizzbuzz leetcode

--- leetcode/Fizz_Buzz.js
+++ leetcode/Fizz_Buzz.js
@@ -1,22 +1,22 @@
 var fizzBuzz = function(n) {
     let array=[]
-    for (let index = 1; index <= 10; index++) {
+    for (let index = 1; index <= n; index++)
+    var str =" "
+    {
         if (index % 3==0 && index % 5==0) {
-            array.push("FizzBuzz")     
+            array.push(str=str+"FizzBuzz")     
         }
         else if (index % 3 == 0){
-            array.push("Fizz") 
+            array.push(str=str+"Fizz") 
         }
         else if (index % 5 == 0){
-            array.push("Buzz") 
+            array.push(str=str+"Buzz") 
         }
         else{
-            array.push(index)
+            array.push(str=str+index)
         }
+        array[idex]=str
     }
         return array
-    
     }
-    
-
 console.log(fizzBuzz(10))
